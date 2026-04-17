2e121fe8082df849916c03f2d1edb1d43d347a2b
Yuliia Yarova <yuliiayarova96@gmail.com>
2026-04-17 23:48:54 +0300

    add styles to Camperlist

--- components/CamperList/CamperList.tsx
+++ components/CamperList/CamperList.tsx
@@ -2,6 +2,8 @@ import { Camper } from "@/types/camper";
 import css from "./CamperList.module.css";
 import Image from "next/image";
 import Button from "../Button/Button";
+import { BsFuelPump, BsDiagram3 } from "react-icons/bs";
+import { FaCar } from "react-icons/fa6";
 
 interface CamperListProps {
   campers: Camper[];
@@ -18,17 +20,36 @@ export default function CamperList({ campers }: CamperListProps) {
             height={240}
             className={css.camperImage}
           />
+
           <div className={css.camperInfo}>
-            <h2 className={css.camperName}>{camper.name}</h2>
-            <p className={css.camperPrice}>{camper.price}</p>
+            <div className={css.camperHeader}>
+              <h2 className={css.camperName}>{camper.name}</h2>
+              <p className={css.camperPrice}>€{camper.price}</p>
+              <div className={css.meta}>
+                <span>
+                  {camper.rating} {camper.totalReviews}
+                </span>
+                <span>{camper.location}</span>
+              </div>
+            </div>
+
+            <p className={css.camperDescr}>{camper.description}</p>
+
+            <ul className={css.tagList}>
+              <li className={css.tagItem}>
+                <BsFuelPump className={css.tagIcon} />
+                {camper.engine}
+              </li>
+              <li className={css.tagItem}>
+                <BsDiagram3 className={css.tagIcon} /> {camper.transmission}
+              </li>
+              <li className={css.tagItem}>
+                <FaCar className={css.tagIcon} />
+                {camper.form}
+              </li>
+            </ul>
+            <Button href={`/catalog/${camper.id}`} text="Show more" />
           </div>
-          <p>
-            {camper.rating} {camper.totalReviews}
-          </p>
-          <p>{camper.location}</p>
-          <p>{camper.description}</p>
-          <span>{camper.amenities}</span>
-          <Button href={`/catalog/${camper.id}`} text="Show more" />
         </li>
       ))}
     </ul>
